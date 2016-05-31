c9722c95f6a7fea388ab9a17b7362cd855b07cc9
Lorenzo Sinisi <lasslo.net@gmail.com>
2016-05-31 20:27:41 +0200

    created the first release (DEV)

--- mix.exs
+++ mix.exs
@@ -37,7 +37,8 @@ defmodule Splurty.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.9"},
      {:ecto, "~> 1.0"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:exrm, "~> 0.14.2" }]
   end
 
   # Aliases are shortcut or tasks specific to the current project.
